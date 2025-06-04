3778ae56be97314cb4fca164ed3c8e91db6672c1
viidit678 <viditthakuri944@gmail.com>
2025-06-04 16:14:20 +0545

    Add conditional operator examples

---
+++ 04conditionaloperator.js
@@ -0,0 +1,17 @@
+let temperature = 44; // at a kohalpur
+// var result =(temperature <20);
+// console.log(result); // true or false based on the temperature
+if (temperature < 10) {
+
+  console.log("snowing");
+}
+ else if (temperature >= 10 && temperature < 20) {
+  console.log("it is cold outside");
+} 
+else if (temperature >= 20 && temperature < 30) {
+  console.log("it is moderate temperature outside");
+} 
+else {
+  console.log("it is very hot outside");
+}
+// conditional operator
