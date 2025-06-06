c69c66652c50460bd418d85cafdc51b1e8cde9b2
viidit678 <viditthakuri944@gmail.com>
2025-06-06 16:30:34 +0545

    add fuctions.js

---
+++ fuctions.js
@@ -0,0 +1,18 @@
+function  sayHello(name){
+    console.log ("hello,viidit");
+    console.log ("hello,name");
+    console.log(`hello,${name}.how are you doing?`);
+
+}   
+sayHello();
+sayHello();
+sayHello("viidit");
+sayHello("viidit thakuri");
+function namstey(){
+    return "hello world";
+}
+namstey();
+var greetings = namstey();
+console.log(greetings);
+console.log(namstey());
+console.log(namstey);
