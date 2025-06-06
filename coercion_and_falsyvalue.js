f92251ab6a8b9d7b1dbc4b0b8caa6a611f17da45
viidit678 <viditthakuri944@gmail.com>
2025-06-06 15:53:20 +0545

    coercion_and_falsyvalue.js

--- coercion_and_falsyvalue.js
+++ coercion_and_falsyvalue.js
@@ -44,3 +44,13 @@ if (user){
     console.log("0"+0);//10 is a string so it will concatenate and give 00
 
 //} 
+var user ="1"; //it is  trythy value
+if (1==user){    
+    console.log("condition is true");//10 is a string so it will concatenate and give 00
+
+} //it is coercion
+var user = "1";
+if (1===user){    
+    console.log("condition is true");//10 is a string so it will concatenate and give 00
+
+} 
